dcc79f7c483a67d3ebe0fcd73fdc856a0cc588c9
Gabe Bravo <gabecbravo@gmail.com>
2020-02-16 16:27:05 -0500

    Adde the other page contexts

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,7 @@
 import { HomeContext } from '../views/home/HomeProvider';
 import { Page1Context } from '../views/page1/Page1Provider';
+import { Page2Context } from '../views/page2/Page2Provider';
+import { Page3Context } from '../views/page3/Page3Provider';
 
 export const transformPathName = pn => {
   const pathArr = pn.split('/');
@@ -11,6 +13,10 @@ export const getContext = pn => {
   switch (pathArr[1]) {
     case 'page1':
       return Page1Context;
+    case 'page2':
+      return Page2Context;
+    case 'page3':
+      return Page3Context;
     default:
       return HomeContext;
   }
